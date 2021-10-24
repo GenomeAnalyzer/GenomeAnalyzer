6ed531a6e06235e8bfb298fd00a027d36a18b3e0
fm16191 <56755856+fm16191@users.noreply.github.com>
2021-10-24 19:05:04 +0200

    Initial Commit - Algorithms - Add basic code blocs for algorithms

---
+++ gene.c
@@ -0,0 +1,52 @@
+
+
+//////////////// Generating mRNA
+void generating_mRNA(){
+    // for (unsigned long long i = 0; i < seq_len; i++)
+    //     if (seq[i] == 'T')
+    //         seq[i] = 'U';
+}
+
+
+//////////////// Detecting genes
+void detecting_genes(){
+    // struct gene_map_s {
+    //     //
+    //     unsigned long long genes_counter;
+    //     //Gene start position (AUG)
+    //     unsigned long long gene_start[MAX_GENES];
+    //     //Gene stop position (UAA, UAG, UGA)
+    //     unsigned long long gene_end[MAX_GENES];
+    // };
+    // gene_map_s gene_map;
+
+
+// Algorithm:
+    // ----------
+    //     start_pos = lookup AUG in seq;
+    // stop_pos = lookup(UAA or UAG or UGA) in seq + start_pos;
+    // if (start_pos && stop_pos)   {
+    //     gene_map.gene_start[gene_map.genes_counter) = start_pos;
+    //     gene_map.gene_stop[gene_map.genes_counter) = stop_pos;
+}
+
+//////////////// Generating an amino acid chain (protein) 
+void generating_amino_acid_chain(){
+
+}
+
+//////////////// Detecting probable mutation zones
+void detecting_mutations(){
+
+}
+
+//////////////// Calculating the matching score of two sequences
+void calculating_matching_score(){
+
+}
+
+//////////////// Hamming calculation
+void hamming(){
+    // for (i = 0; i < min(lengt(seq0), lenghth(seq1)); i++)
+    //     d += popcount(seq0[i] xor seq1[i]);
+}
