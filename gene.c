c5e9e7256e1802af27790cef3c48550c6d363863
Candice Astier <candice.astier@ens.uvsq.fr>
2021-11-10 10:49:36 +0100

    Algo : Generating ARN
    Add of the algo which convert a binary DNA sequence to a string mRNA sequence.

--- gene.c
+++ gene.c
@@ -1,10 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 
 //////////////// Generating mRNA
-void generating_mRNA(){
-    // for (unsigned long long i = 0; i < seq_len; i++)
-    //     if (seq[i] == 'T')
-    //         seq[i] = 'U';
+/**
+ * generating_mRNA - Convert a binary DNA sequence to a string mRNA sequence
+ * dna_seq      array of binary
+ * seq_len      size of dna_seq
+ * 
+ * rna_seq      array of char
+ */
+char *generating_mRNA(int *dna_seq, int seq_len){
+    // Check the input argument
+    if(!dna_seq)
+        return printf("ERROR: generating_mRNA: undefined sequence\n"), NULL;
+
+    // Create and check the output
+    char *rna_seq = NULL;
+    rna_seq = malloc(sizeof(*rna_seq) * (seq_len / 2));
+    if(!rna_seq)
+        return printf("ERROR: generating_mRNA: cannot allocate memory\n"), NULL;
+
+    int j = 0;
+    // Parse the binary DNA sequence two by two
+    for (unsigned long long i = 0; i < seq_len; i += 2){
+        switch(dna_seq[i]){
+            case 0:
+                if(dna_seq[i + 1] == 0)
+                    rna_seq[j] = 'A';
+                else if(dna_seq[i + 1] == 1)
+                    rna_seq[j] = 'G';
+                break;
+            case 1:
+                if(dna_seq[i + 1] == 0)
+                    rna_seq[j] = 'C';
+                else if(dna_seq[i + 1] == 1)
+                    rna_seq[j] = 'U';
+                break;
+            default:
+                return printf("ERROR: generating_mRNA: invalid value in DNA sequence\n"), NULL;
+        }
+        j++;
+    }
+    return rna_seq;
 }
 
 
