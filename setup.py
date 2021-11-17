1ea7be2582248239e5c95551eeb49a154dbd79f7
71xela <91948050+71xela@users.noreply.github.com>
2021-11-17 21:01:21 +0100

    Setup of C extension in Python
    Change commit description

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup, Extension
 DNA_module = Extension("DNA", sources = [ "gene.c", "DNA.c" ])
 
 setup(name        = "DNA",
-      version     = "0.1",
+      version     = "1.0",
       description = "Genome analyser", 
       ext_modules = [ DNA_module ])
