df38bdd9ad0b8bf503a13cbe974d57ccf13cb333
fm16191 <56755856+fm16191@users.noreply.github.com>
2021-11-18 22:39:57 +0100

    Update generating_amino_acid_chain algorithm
    - Update algorithm using the json "codons_list.json" into a "codon" array by the python calling program

--- gene.h
+++ gene.h
@@ -15,18 +15,17 @@ typedef struct gene_map_s {
 }gene_map_t;
 
 typedef struct codon {
-    int number_of_codons;
-    char* codons[3];
+    char* codon;
     char* short_name;
-    char symbol;
     char* full_name;
+    char symbol;
 }codon;
 
 //extern struct gene_map_s gene_map;
 
 char* generating_mRNA(int* dna_seq, int seq_len);
 void  detecting_genes(unsigned int gene [], gene_map_t* gene_map);
-char* generating_amino_acid_chain(char* seq);
+char* generating_amino_acid_chain(char* seq, int seq_size, int codons_count, codon codons []);
 bool detecting_mutations(const unsigned int gene_seq [], const unsigned long long size_sequence);
 float calculating_matching_score(int sequence_size, int seq1 [], int seq2 []);
 int binary_size_count(int b);
