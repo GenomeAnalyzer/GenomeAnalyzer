e98829055854257959e8ae2aed541043d60cbcdb
ZzDaBing <guillaume.bigand@ens.uvsq.fr>
2021-11-18 19:25:17 +0100

    Add comments in test : detecting_mutations function

--- test_gene.c
+++ test_gene.c
@@ -6,10 +6,15 @@
 
 #include "gene.c"
 
+//Tests for detecting_mutations function
 static void test_detecting_mutations(void ** state){
 
+  //The function should return true if we give in input an array with a 0,1 or 1,0 continuous sequence
+  //higher than 1/5th of the half size of the array.
   assert_true(detecting_mutations((int[]){0,1,0,1,0,1,1,1,1,1,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,1,1,1,0,0
                                     ,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,0,1}, 60));
+  //The function should return false if we give in input an array with a 0,1 or 1,0 continuous sequence
+  //lower than 1/5th of the half size of the array.
   assert_false(detecting_mutations((int[]){0,1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,1,0,0,1,1,1,1,1,0,0
                                     ,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,0,1}, 60));
 }
