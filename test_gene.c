b31551a85f187a9f098cad9378fbf61ced4cc4fc
Candice Astier <candice.astier@ens.uvsq.fr>
2021-11-21 19:41:04 +0100

    Add of an unit test : test_generating_mRNA

--- test_gene.c
+++ test_gene.c
@@ -6,6 +6,21 @@
 
 #include "gene.c"
 
+static void test_generating_mRNA(void ** state){
+  // Test if the algorithm is OK
+  assert_string_equal("AUGCGUGGGUAG",
+                      generating_mRNA((unsigned int[]){0,0,1,1,0,1,1,0,0,1,1,1,0,1,0,1,0,1,1,1,0,0,0,1},
+                                      24));
+
+  // Test whether the function correctly detects errors:
+  // --- NULL error
+  assert_ptr_equal(NULL, generating_mRNA(NULL, 0));
+  // --- invalid value in gene_seq
+  assert_ptr_not_equal("AUGCGUGGGUAG",
+                       generating_mRNA((unsigned int[]){0,0,1,44,0,1,1,0,0,1,1,1,0,1,0,1,0,1,1,1,0,0,0,1},
+                                       24));
+}
+
 //Tests for detecting_mutations function
 static void test_detecting_mutations(void ** state){
 
@@ -22,6 +37,7 @@ static void test_detecting_mutations(void ** state){
 int main(void) {
   int result = 0;
   const struct CMUnitTest tests[] = {
+      cmocka_unit_test(test_generating_mRNA),
       cmocka_unit_test(test_detecting_mutations),
   };
   result |= cmocka_run_group_tests_name("gene", tests, NULL, NULL);
