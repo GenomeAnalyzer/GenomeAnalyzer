9fc1f8a6de5fa3ecd5247c49bce0b852d4ebbd50
Fiona Santoro <fsantoro39@gmail.com>
2021-12-20 23:58:58 +0100

    Feat: Modify main.py
    * Add output to rapport.txt
    
    * Add comparison in the same sequence

--- main.py
+++ main.py
@@ -14,25 +14,31 @@ chain = [[[]]]
 
 mutation = [[[]]]
 
-matching = [[[]]]
+matching = [[]]
 #gene = [[]]
+
+f = open("rapport.txt", "a")
+
 for file in glob.glob("fastas/*.fasta"):
+    f.write("######################"+file+"######################")
 
     read = m.read_file(file)
 
-    sequence[i] = DNA.convert_to_binary(read,len(read))
+    sequence.append(DNA.convert_to_binary(read,len(read)))
 
-    gene[i] = DNA.detecting_genes(array.array('I',sequence[i]))
+    gene.append(DNA.detecting_genes(array.array('I',sequence[i])))
 
     for j in range (len(gene[i])-1):
-         mrna[i].append(DNA.generating_mRNA(array.array('i',sequence[i][gene[i][j][0]:(gene[i][j][1])])))
-         chain[i].append(DNA.generating_amino_acid_chain(array.array('i',sequence[i][gene[i][j][0]:(gene[i][j][1])])))
-       
+         mrna.append((DNA.generating_mRNA(array.array('i',sequence[i][gene[i][j][0]:(gene[i][j][1])]))))
+         #chain[i].append(DNA.generating_amino_acid_chain(array.array('i',sequence[i][gene[i][j][0]:(gene[i][j][1])])))
+    f.write("\nMRNA:")
+    f.write(str(mrna))
 
-
+    f.write("\nMatching:")
     for j in range((int((len(gene[i])-1)/2)), -1, -1):
         for k in range((int((len(gene[i])-1)/2))):
-                matching[i].append(DNA.calculating_matching_score(array.array('H',gene[i][j]),array.array('H',gene[i][k])))
-
-
-    i+=1
+                f.write("["+str(j)+","+str(k)+"]=")
+                matching.append((DNA.calculating_matching_score(array.array('H',gene[i][j]),array.array('H',gene[i][k]))))
+                f.write(str(matching[k])+"\t")
+    i=i+1
+f.close()
