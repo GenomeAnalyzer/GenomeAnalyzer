257f09ec6e706f3890cfb3ae6893fb058edbb9ef
fm16191 <56755856+fm16191@users.noreply.github.com>
2021-12-30 13:58:57 +0100

    Add test_DNA_bin.py get_binary_value test
    - Test for first value
    - Test for leading 0s
    - Test for all binary values from 00000 to 11111

--- test_DNA_bin.py
+++ test_DNA_bin.py
@@ -6,11 +6,17 @@ import moduleDNA as m
 ###### Verify if C extension in Python is working #####
 
 def test_get_binary_value():
-	res = DNA_bin.get_binary_value(1,0)
-	assert res == 1
-	res = DNA_bin.get_binary_value(1,17)
-	assert res == 0
+	# Test if the algorithm is OK
+	# 1 = 0000000000000000000000000000001
+	assert 1 == DNA_bin.get_binary_value(array.array('I', [1]),0)
+	# assert leading 0
+	assert 0 == DNA_bin.get_binary_value(array.array('I', [1]),17)
 
-
-
-
+  	# Pour chaque binaire de 00000 à 11111, vérifier chaque bit.
+	for i in range(32):
+		seq_bin = array.array('I', [i])
+		assert i%2 == DNA_bin.get_binary_value(seq_bin,0)
+		assert int(i/2%2) == DNA_bin.get_binary_value(seq_bin,1)
+		assert int(i/4%2) == DNA_bin.get_binary_value(seq_bin,2)
+		assert int(i/8%2) == DNA_bin.get_binary_value(seq_bin,3)
+		assert int(i/16%2) == DNA_bin.get_binary_value(seq_bin,4)
