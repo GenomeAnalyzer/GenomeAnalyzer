ff2a779013647b3fcb36a025ea5d1407b46821fc
fm16191 <56755856+fm16191@users.noreply.github.com>
2021-12-30 14:40:45 +0100

    Add test_DNA_bin.py change_binary_value test
    - Add tests for double change on a binary of size 7

--- test_DNA_bin.py
+++ test_DNA_bin.py
@@ -20,3 +20,16 @@ def test_get_binary_value():
 		assert int(i/4%2) == DNA_bin.get_binary_value(seq_bin,2)
 		assert int(i/8%2) == DNA_bin.get_binary_value(seq_bin,3)
 		assert int(i/16%2) == DNA_bin.get_binary_value(seq_bin,4)
+
+def test_change_binary_value():
+	# Test if the algorithm is OK
+
+  	# 85 = 1010101
+	seq_bin = array.array('I', [85])
+	# Invert values of seq_bin
+	for i in range(0,7): DNA_bin.change_binary_value(seq_bin, i, i%2)
+	# 42 = 0101010
+	assert 42 == seq_bin[0]
+	# Invert back values of seq_bin
+	for i in range(0,7): DNA_bin.change_binary_value(seq_bin, i, int((i+1)%2))
+	assert 85 == seq_bin[0]
