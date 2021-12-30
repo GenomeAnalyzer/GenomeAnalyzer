9c519348fde0db6b3263d5d9d39f90a3d348a61b
Fiona Santoro <fsantoro39@gmail.com>
2021-12-30 15:08:50 +0100

    TODO: Fix error on mutation (malloc)

--- main.py
+++ main.py
@@ -18,31 +18,34 @@ mutation = [[[]]]
 matching = [[]]
 #gene = [[]]
 
-f = open("rapport.txt", "a")
+f = open("rapport.txt", "w")
 
 for file in glob.glob("fastas/*.fasta"):
-    f.write("######################"+file+"######################")
+    f.write("\n######################"+file+"######################")
 
     read = m.read_file(file)
 
     sequence.append(DNA.convert_to_binary(read,len(read)))
+    #print(str(sequence[i]))
 
+    #gene.append(DNA.detecting_genes(array.array('L',sequence[i])))
     gene.append(DNA.detecting_genes(array.array('H',sequence[i])))
-    print(str(len(gene[i])))
-
 
     f.write("\nMRNA\t\t\t\t\t\t\t\tCHAIN\t\t\t\t\t\t\t\tMUTATION\n")
     for j in range (len(gene[i])-1):
+        #print('gene')       
         res = (DNA.generating_mRNA(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])])))
         if res:
             mrna.append(res.decode("cp1252", "replace"))
             f.write(str(res.decode("cp1252", "replace")))
+        #print('aminoacid ='+str(sequence[i][gene[i][j][0]:(gene[i][j][1])]))
         res2 =  DNA.generating_amino_acid_chain(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])]))
 
         if res2:
             chain.append(res2.decode("cp1252", "replace"))
             f.write("\t\t\t\t\t\t\t\t"+str(res2.decode("cp1252", "replace")))
-
+        #print('ok')
+        #print('mutation =')
         mutres = DNA.detecting_mutations(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])]))
         mutation.append(mutres)
         f.write("\t\t\t\t\t\t\t\t"+str(mutres)+"\n")
