e19cc64c0f4a55c99e2809f0669e2583faa26342
fm16191 <56755856+fm16191@users.noreply.github.com>
2021-12-31 04:22:39 +0100

    Fix DNA_bin.c / generating_mRNA interface
    - Add binary array size calculation

--- DNA_bin.c
+++ DNA_bin.c
@@ -323,8 +323,20 @@ static PyObject* DNAb_generating_mRNA(PyObject* self, PyObject* args) {
 		return NULL;
 	}
 
+	unsigned int* res, res2;
+	unsigned int size = 0;
+	for (int i = 0; i < view.len / view.itemsize; i++) {
+		res = view.buf + i * view.itemsize;
+		memcpy(&res2, res, sizeof(res));
+		while (res2 > 0) {
+			res2 = res2 / 2;
+			size += 1;
+		}
+		if (size % 2 != 0) size++;
+	}
+
 	//Return the char* value as a Python string object
-	return Py_BuildValue("y", generating_mRNA(view.buf, view.shape[0]));
+	return Py_BuildValue("y", generating_mRNA(view.buf, size));
 }
 
 //////////////// Detecting genes
