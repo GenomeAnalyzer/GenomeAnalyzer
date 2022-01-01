cf2dd19f3ea714db3320d5574419399c350cd665
Fiona Santoro <fsantoro39@gmail.com>
2022-01-01 02:24:03 +0100

    Style: improve ouput

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import DNA
 import array
 import glob
 import moduleDNA as m
+import os
 
 
 read = []
 sequence = [[]]
 mrna = [[[]]]
 i = 0
-gene = [[[]]]
+gene = list((()))
 
 chain = [[[]]]
 
@@ -18,9 +19,17 @@ mutation = [[[]]]
 matching = [[]]
 #gene = [[]]
 
-f = open("rapport.txt", "w")
-fh = open('rapport.html','w')
-message = """<html>
+try:
+    # Create target Directory
+    os.makedirs("output/sequences")
+
+    print("Output directory created ") 
+except FileExistsError:
+    print("Output directory already exists")
+
+f = open("output/rapport.txt", "w")
+fh = open('output/rapport.html','w')
+start = """<html>
 <head><style> 
 th, td {
   font-size: 10px; 
@@ -56,92 +65,115 @@ details > summary {
 
 """
 
-fh.write(message)
+fh.write(start)
 for file in glob.glob("fastas/*.fasta"):
-    message = "<details><summary>"+file+"</summary>"
+    message = "<details><summary>"+str(file)+"</summary>"
+    fh.write(message+"<a href=\"sequences/"+str(file).replace("fastas/","")+".html\">"+str(file)+"</a></details>")
+    
     f.write("\n######################"+file+"######################")
+    fhtmp = open("output/sequences/"+str(file).replace("fastas/","")+'.html','w')
+
+    fhtmp.write(start+ "<h1>"+str(file)+"</h1><a href=\"../rapport.html\" target=\"_blank\"><input type=\"button\" value=\"Retour\"></a>")
 
 
-
-    message += "<table>\n<tbody>\n<tr>\n<td class = \"title\">Sequence</td>\n<td class = \"title\">MRNA</td>\n<td class = \"title\">Chain</td>\n<td class = \"title\">Mutation</td>\n</tr>\n"
+    message = "<table>\n<tbody>\n<tr>\n<td class = \"title\">Sequence</td>\n<td class = \"title\">MRNA</td>\n<td class = \"title\">Chain</td>\n<td class = \"title\">Mutation</td>\n</tr>\n"
     
 
     read = m.read_file(file)
 
     sequence.append(DNA.convert_to_binary(read,len(read)))
-    #print(str(sequence[i]))
+    #print(len(sequence[i]))
 
+    gen =DNA.detecting_genes(array.array('L',sequence[i]))
     gene.append(DNA.detecting_genes(array.array('L',sequence[i])))
-    #gene.append(DNA.detecting_genes(array.array('H',sequence[i])))
 
-    f.write("\nMRNA\t\t\t\t\t\t\t\tCHAIN\t\t\t\t\t\t\t\tMUTATION\n")
+    if len(gene[i]) > 0:
+      print("gene 1 = "+str(gene[i][0]))
+      print("gene 2 = "+str(gen[0]))
+
+    #f.write("\nMRNA\t\t\t\t\t\t\t\tCHAIN\t\t\t\t\t\t\t\tMUTATION\n")
     for j in range (len(gene[i])-1):
-        message += "<tr><td>"+str(sequence[i][gene[i][j][0]:(gene[i][j][1])])+ "</td>\n"
-        #print('gene')       
+        message += "<tr><td>"+str(sequence[i][gene[i][j][0]:(gene[i][j][1])+1])+ "</td>\n"
         res = (DNA.generating_mRNA(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])])))
         if res:
             mrna.append(res.decode("cp1252", "replace"))
-            f.write(str(res.decode("cp1252", "replace")))
+            #f.write(str(res.decode("cp1252", "replace")))
             message+="<td>"+str(res.decode("cp1252", "replace"))+ "</td>\n"
+            #print(res.decode("cp1252", "replace"))
         else:
             message+="<td> none </td>\n"
+          
 
         #print('aminoacid ='+str(sequence[i][gene[i][j][0]:(gene[i][j][1])]))
         res2 =  DNA.generating_amino_acid_chain(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])]))
 
         if res2:
             chain.append(res2.decode("cp1252", "replace"))
-            f.write("\t\t\t\t\t\t\t\t"+str(res2.decode("cp1252", "replace")))
+            #f.write("\t\t\t\t\t\t\t\t"+str(res2.decode("cp1252", "replace")))
             message+="<td>"+str(res2.decode("cp1252", "replace"))+ "</td>\n"
         else:
             message+="<td> none </td>\n"            
         #print('ok')
-        #print('mutation =')
         mutres = DNA.detecting_mutations(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])]))
         mutation.append(mutres)
-        message+="<td>"+str(mutres)+ "</td>\n</tr>\n"
-        f.write("\t\t\t\t\t\t\t\t"+str(mutres)+"\n")
+        if len(mutres) == 0:
+          message+="<td>none</td>\n</tr>\n"
+        else:
+          message+="<td>"+str(mutres)+ "</td>\n</tr>\n"
+        #f.write("\t\t\t\t\t\t\t\t"+str(mutres)+"\n")
     message+= "</tbody>\n</table>\n"
 
+    #print(str(gene))
 
 
-    #f.write("\nMRNA:")
-    #f.write(str(mrna))
-    #f.write("\nCHAIN:")
-    #f.write(str(chain))    
-    #f.write("\nMUTATION:")
-    #f.write(str(mutation))
+
+    f.write("\nMRNA:")
+    f.write(str(mrna))
+    f.write("\nCHAIN:")
+    f.write(str(chain))    
+    f.write("\nMUTATION:")
+    f.write(str(mutation))
     message += "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
 
     f.write("\nMatching:")
     if (len(gene[i])-1 > 2):
         for j in range((int((len(gene[i]))/2)), -1, -1):
             for k in range((int((len(gene[i]))/2))):     
-                message+="<tr><td>"+str(gene[i][j])+","+str(gene[i][k])+ "</td>\n"
-                f.write("["+str(gene[i][j])+","+str(gene[i][k])+"]=")
-                res = (DNA.calculating_matching_score(array.array('H',gene[i][j]),array.array('H',gene[i][k])))
+                message+="<tr><td>"+str(sequence[i][gene[i][j][0]:gene[i][j][1]])+" , "+str(sequence[i][gene[i][k][0]:gene[i][k][1]])+ "</td>\n"
+                f.write("["+str(gene[i][j][0])+","+str(gene[i][k][0])+"]=")
+                res = (DNA.calculating_matching_score(array.array('H',sequence[i][gene[i][j][0]:gene[i][j][1]]),array.array('H',sequence[i][gene[i][k][0]:gene[i][k][1]])))
                 matching.append(res)
                 message+="<td>"+str(res)+ "</td> \n</tr>\n"
                 f.write(str(res)+"\t")
-    message+= "</tbody>\n</table>\n</details>\n"  
+    message+= "</tbody>\n</table>\n</details>\n </html>"  
 
 
-    fh.write(message)
+    fhtmp.write(message)
+    fhtmp.close()
     i=i+1
-message = "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
+    if i ==4:
+      break
 
+
+message= " <a href=\"comp.html\">Comparaison séquences</a></details></html>"  
+fh.write(message)
+fh.close()
+
+fh = open('output/comp.html','w')
+fh.write(start+ "<h1>Comparaison entre séquences</h1><a href=\"rapport.html\" target=\"_blank\"><input type=\"button\" value=\"Retour\"></a>")
+message = "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
 f.write("\nMatching:")
-for i in range((int((len(sequence))/2)), -1, -1):
-    for c in range((int((len(sequence))/2))):     
-        for j in (int((len(sequence[i])))):
-            for k in (int((len(sequence[c])))):
-                message+="<tr><td>"+str(gene[i][j])+","+str(gene[c][k])+ "</td>\n"
-                f.write("["+str(gene[i][j])+","+str(gene[c][k])+"]=")
-                res = (DNA.calculating_matching_score(array.array('H',gene[i][j]),array.array('H',gene[c][k])))
+for i in range((int((len(gene))/2)), -1, -1):
+    for c in range((int((len(gene))/2))):     
+        for j in range(int((len(gene[i])))):
+            for k in range(int((len(gene[c])))):
+                message+="<tr><td>"+str(sequence[i][gene[i][j][0]:gene[i][j][1]])+" , "+str(sequence[c][gene[c][k][0]:gene[c][k][1]])+ "</td>\n"
+                f.write("["+str(gene[i][j][0])+","+str(gene[c][k])+"]=")
+                res = (DNA.calculating_matching_score(array.array('H',sequence[i][gene[i][j][0]:gene[i][j][1]]),array.array('H',sequence[c][gene[c][k][0]:gene[c][k][1]])))
                 matching.append(res)
                 message+="<td>"+str(res)+ "</td> \n</tr>\n"
                 f.write(str(res)+"\t")
-message+= "</tbody>\n</table>\n</details>\n"  
+message+= "</tbody>\n</table>\n\n"  
 
 message+= "</html>"  
 fh.write(message)
--- main_bin.py
+++ main_bin.py
@@ -3,13 +3,14 @@ import DNA_bin
 import array
 import glob
 import moduleDNA as m
+import os
 
 
 read = []
 sequence = [[]]
 mrna = [[[]]]
 i = 0
-gene = [[[]]]
+gene = list((()))
 
 chain = [[[]]]
 
@@ -18,9 +19,17 @@ mutation = [[[]]]
 matching = [[]]
 #gene = [[]]
 
-f = open("rapport_bin.txt", "w")
-fh = open('rapport_bin.html','w')
-message = """<html>
+try:
+    # Create target Directory
+    os.makedirs("output/sequences")
+
+    print("Output directory created ") 
+except FileExistsError:
+    print("Output directory already exists")
+
+f = open("output/rapport_bin.txt", "w")
+fh = open('output/rapport_bin.html','w')
+start = """<html>
 <head><style> 
 th, td {
   font-size: 10px; 
@@ -56,112 +65,115 @@ details > summary {
 
 """
 
-fh.write(message)
+fh.write(start)
 for file in glob.glob("fastas/*.fasta"):
-    message = "<details><summary>"+file+"</summary>"
+    message = "<details><summary>"+str(file)+"</summary>"
+    fh.write(message+"<a href=\"sequences/"+str(file).replace("fastas/","")+"_bin.html\">"+str(file)+"</a></details>")
+    
     f.write("\n######################"+file+"######################")
+    fhtmp = open("output/sequences/"+str(file).replace("fastas/","")+'_bin.html','w')
+
+    fhtmp.write(start+ "<h1>"+str(file)+"</h1><a href=\"../rapport_bin.html\" target=\"_blank\"><input type=\"button\" value=\"Retour\"></a>")
 
 
-
-    message += "<table>\n<tbody>\n<tr>\n<td class = \"title\">Sequence</td>\n<td class = \"title\">MRNA</td>\n<td class = \"title\">Chain</td>\n<td class = \"title\">Mutation</td>\n</tr>\n"
-    print("ou")
+    message = "<table>\n<tbody>\n<tr>\n<td class = \"title\">Sequence</td>\n<td class = \"title\">MRNA</td>\n<td class = \"title\">Chain</td>\n<td class = \"title\">Mutation</td>\n</tr>\n"
+    
 
     read = m.read_file(file)
-    print("ok")
-    #print(read)
+
     sequence.append(DNA_bin.convert_to_binary(read,len(read)))
-    print(str(sequence[i]))
+    #print(len(sequence[i]))
 
-    gene.append(DNA_bin.detecting_genes(array.array('I',sequence[i])))
-    print(str(gene[i]))
-  
-    #gene.append(DNA_bin.detecting_genes(array.array('H',sequence[i])))
+    gen =DNA_bin.detecting_genes(array.array('l',sequence[i]))
+    gene.append(DNA_bin.detecting_genes(array.array('l',sequence[i])))
 
-    f.write("\nMRNA\t\t\t\t\t\t\t\tCHAIN\t\t\t\t\t\t\t\tMUTATION\n")
-    for j in range (len(gene[i])):
-        if gene[i][j]:
-            message += "<tr><td>"+str(sequence[i][gene[i][j][0]:(gene[i][j][1])])+ "</td>\n"
-            #print('gene') 
-            print("ici?")
-        
-            res = (DNA_bin.generating_mRNA(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])])))
-            if res:
-                mrna.append(res.decode("cp1252", "replace"))
-                f.write(str(res.decode("cp1252", "replace")))
-                message+="<td>"+str(res.decode("cp1252", "replace"))+ "</td>\n"
-            else:
-                message+="<td> none </td>\n"
+    if len(gene[i]) > 0:
+      print("gene 1 = "+str(gene[i][0]))
+      print("gene 2 = "+str(gen[0]))
 
-            #print('aminoacid ='+str(sequence[i][gene[i][j][0]:(gene[i][j][1])]))
-            res2 =  DNA_bin.generating_amino_acid_chain(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])]))
+    #f.write("\nMRNA\t\t\t\t\t\t\t\tCHAIN\t\t\t\t\t\t\t\tMUTATION\n")
+    for j in range (len(gene[i])-1):
+        print(str(sequence[i][gene[i][j][0]/31:(gene[i][j][1]/31)]))
+        message += "<tr><td>"+str(sequence[i][gene[i][j][0]:(gene[i][j][1])])+ "</td>\n"
+        res = (DNA_bin.generating_mRNA(array.array('l',sequence[i][gene[i][j][0]:(gene[i][j][1])])))
+        if res:
+            mrna.append(res.decode("cp1252", "replace"))
+            #f.write(str(res.decode("cp1252", "replace")))
+            message+="<td>"+str(res.decode("cp1252", "replace"))+ "</td>\n"
+            #print(res.decode("cp1252", "replace"))
+        else:
+            message+="<td> none </td>\n"
+          
 
-            if res2:
-                chain.append(res2.decode("cp1252", "replace"))
-                f.write("\t\t\t\t\t\t\t\t"+str(res2.decode("cp1252", "replace")))
-                message+="<td>"+str(res2.decode("cp1252", "replace"))+ "</td>\n"
-            else:
-                message+="<td> none </td>\n"            
-            #print('ok')
-            #print('mutation =')
-            mutres = DNA_bin.detecting_mutations(array.array('H',sequence[i][gene[i][j][0]:(gene[i][j][1])]))
-            mutation.append(mutres)
-            message+="<td>"+str(mutres)+ "</td>\n</tr>\n"
-            f.write("\t\t\t\t\t\t\t\t"+str(mutres)+"\n")
+        #print('aminoacid ='+str(sequence[i][gene[i][j][0]:(gene[i][j][1])]))
+        res2 =  DNA_bin.generating_amino_acid_chain(array.array('l',sequence[i][gene[i][j][0]:(gene[i][j][1])]))
+
+        if res2:
+            chain.append(res2.decode("cp1252", "replace"))
+            #f.write("\t\t\t\t\t\t\t\t"+str(res2.decode("cp1252", "replace")))
+            message+="<td>"+str(res2.decode("cp1252", "replace"))+ "</td>\n"
+        else:
+            message+="<td> none </td>\n"            
+        #print('ok')
+        mutres = DNA_bin.detecting_mutations(array.array('l',sequence[i][gene[i][j][0]:(gene[i][j][1])]))
+        mutation.append(mutres)
+        message+="<td>"+str(mutres)+ "</td>\n</tr>\n"
+        #f.write("\t\t\t\t\t\t\t\t"+str(mutres)+"\n")
     message+= "</tbody>\n</table>\n"
 
-    print("ici?")
+    #print(str(gene))
 
 
-    #f.write("\nMRNA:")
-    #f.write(str(mrna))
-    #f.write("\nCHAIN:")
-    #f.write(str(chain))    
-    #f.write("\nMUTATION:")
-    #f.write(str(mutation))
+
+    f.write("\nMRNA:")
+    f.write(str(mrna))
+    f.write("\nCHAIN:")
+    f.write(str(chain))    
+    f.write("\nMUTATION:")
+    f.write(str(mutation))
     message += "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
-    print("ici?")
-    print(str(gene[i]))
-   
-    print("aprés")
+
     f.write("\nMatching:")
-    if gene[i]:
-        if (len(gene[i]) > 2):
-            for j in range((int((len(gene[i]))/2)), -1, -1):
-                for k in range((int((len(gene[i]))/2))):  
-                    print("ici matching?")
-    
-                    if gene[i][j]:  
-                        message+="<tr><td>"+str(gene[i][j])+","+str(gene[i][k])+ "</td>\n"
-                        f.write("["+str(gene[i][j])+","+str(gene[i][k])+"]=")
-                        res = (DNA_bin.calculating_matching_score(array.array('H',gene[i][j]),array.array('H',gene[i][k])))
-                        matching.append(res)
-                        message+="<td>"+str(res)+ "</td> \n</tr>\n"
-                        f.write(str(res)+"\t")
-        message+= "</tbody>\n</table>\n</details>\n"  
-    print("ici?")
-
-
-    fh.write(message)
-    i=i+1
-message = "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
-
-f.write("\nMatching:")
-for i in range((int((len(sequence))/2)), -1, -1):
-    for c in range((int((len(sequence))/2))):     
-        for j in (int((len(sequence[i])))):
-            for k in (int((len(sequence[c])))):
-                print("ici matching?")
-
-                message+="<tr><td>"+str(gene[i][j])+","+str(gene[c][k])+ "</td>\n"
-                f.write("["+str(gene[i][j])+","+str(gene[c][k])+"]=")
-                res = (DNA_bin.calculating_matching_score(array.array('H',gene[i][j]),array.array('H',gene[c][k])))
+    if (len(gene[i])-1 > 2):
+        for j in range((int((len(gene[i]))/2)), -1, -1):
+            for k in range((int((len(gene[i]))/2))):     
+                message+="<tr><td>"+str(sequence[i][gene[i][j][0]:gene[i][j][1]])+" , "+str(sequence[i][gene[i][k][0]:gene[i][k][1]])+ "</td>\n"
+                f.write("["+str(gene[i][j][0])+","+str(gene[i][k][0])+"]=")
+                res = (DNA_bin.calculating_matching_score(array.array('l',sequence[i][gene[i][j][0]:gene[i][j][1]]),array.array('l',sequence[i][gene[i][k][0]:gene[i][k][1]])))
                 matching.append(res)
                 message+="<td>"+str(res)+ "</td> \n</tr>\n"
                 f.write(str(res)+"\t")
-message+= "</tbody>\n</table>\n</details>\n"  
+    message+= "</tbody>\n</table>\n</details>\n </html>"  
 
-message+= "</html>"  
+
+    fhtmp.write(message)
+    fhtmp.close()
+    i=i+1
+    if i ==4:
+      break
+message= " <a href=\"comp_bin.html\">Comparaison séquences</a></details></html>"  
 fh.write(message)
-f.close()
 fh.close()
+
+fh = open('output/comp_bin.html','w')
+fh.write(start+ "<h1>Comparaison entre séquences</h1><a href=\"rapport_bin.html\" target=\"_blank\"><input type=\"button\" value=\"Retour\"></a>")
+message = "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
+print(str(len(gene)))
+f.write("\nMatching:")
+for i in range((int((len(gene))/2)), -1, -1):
+    for c in range((int((len(gene))/2))):     
+        for j in range(int((len(gene[i])))):
+            for k in range(int((len(gene[c])))):
+                message+="<tr><td>"+str(sequence[i][gene[i][j][0]:gene[i][j][1]])+" , "+str(sequence[c][gene[c][k][0]:gene[c][k][1]])+ "</td>\n"
+                f.write("["+str(gene[i][j][0])+","+str(gene[c][k])+"]=")
+                res = (DNA_bin.calculating_matching_score(array.array('l',sequence[i][gene[i][j][0]:gene[i][j][1]]),array.array('l',sequence[c][gene[c][k][0]:gene[c][k][1]])))
+                matching.append(res)
+                message+="<td>"+str(res)+ "</td> \n</tr>\n"
+                f.write(str(res)+"\t")
+message+= "</tbody>\n</table>\n\n </html>"  
+
+
+fh.write(message)
+fh.close()
+f.close()
 
