e95dc691fe7c1c8d434ca5822bd88d7ce6fb4eb3
Fiona Santoro <fsantoro39@gmail.com>
2022-01-03 22:57:56 +0100

    Style: Modify main.py main_bin.py for a better html output

--- main_bin.py
+++ main_bin.py
@@ -6,178 +6,158 @@ import moduleDNA as m
 import os
 
 
-read = []
-sequence = [[]]
-mrna = [[[]]]
-i = 0
-gene = list((()))
+def main():
+  read = list()
+  sequence = list()
+  mrna = list()
+  i = 0
+  gene = list()
 
-chain = [[[]]]
+  chain = list()
 
-mutation = [[[]]]
+  mutation = list()
 
-matching = [[]]
-#gene = [[]]
+  matching = list()
 
-try:
-    # Create target Directory
-    os.makedirs("output/sequences")
+  try:
+      # Create target Directory
+      os.makedirs("output/sequences")
 
-    print("Output directory created ") 
-except FileExistsError:
-    print("Output directory already exists")
+      print("Output directory created ") 
+  except FileExistsError:
+      print("Output directory already exists")
 
-f = open("output/rapport_bin.txt", "w")
-fh = open('output/rapport_bin.html','w')
-start = """<html>
-<head><style> 
-th, td {
-  font-size: 10px; 
-}
-.title {
-  font-size: 15px; 
-}
-/*Style du tableau*/
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-  border-style: dashed;
-}
-.title {
-  border-style : dashed dashed dashed solid;
-  padding-left: 1%;
-}
-table {
-  width:90%;
-  margin-left : 5%;
-}
+  fh = open('output/rapport_bin.html','w')
+  start = """<html>
+  <head><style> 
+  th, td {
+    font-size: 10px; 
+  }
+  .title {
+    font-size: 15px; 
+  }
+  /*Style du tableau*/
+  table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    border-style: dashed;
+  }
+  .title {
+    border-style : dashed dashed dashed solid;
+    padding-left: 1%;
+  }
+  table {
+    width:90%;
+    margin-left : 5%;
+  }
 
-details > summary {
-  padding: 4px;
-  width: 200px;
-  background-color: #eeeeee;
-  border: none;
-  box-shadow: 1px 1px 2px #bbbbbb;
-  cursor: help;
-}
-</style>
-</head>
+  details > summary {
+    padding: 4px;
+    width: 200px;
+    background-color: #eeeeee;
+    border: none;
+    box-shadow: 1px 1px 2px #bbbbbb;
+    cursor: help;
+  }
+  </style>
+  </head>
 
-"""
+  """
+  fhc = open('output/comp_bin.html','w')
+  messagematch = start+ "<h1>Comparaison entre séquences</h1><a href=\"rapport_bin.html\" target=\"_blank\"><input type=\"button\" value=\"Retour\"></a>"
 
-fh.write(start)
-for file in glob.glob("fastas/*.fasta"):
-    message = "<details><summary>"+str(file)+"</summary>"
-    fh.write(message+"<a href=\"sequences/"+str(file).replace("fastas/","")+"_bin.html\">"+str(file)+"</a></details>")
-    
-    f.write("\n######################"+file+"######################")
-    fhtmp = open("output/sequences/"+str(file).replace("fastas/","")+'_bin.html','w')
+  fh.write(start)
+  for file in glob.glob("fastas/*.fasta"):
+      
+      fhtmp = open("output/sequences/"+str(file).replace("fastas/","")+'_bin.html','w')
 
-    fhtmp.write(start+ "<h1>"+str(file)+"</h1><a href=\"../rapport_bin.html\" target=\"_blank\"><input type=\"button\" value=\"Retour\"></a>")
+      fhtmp.write(start+ "<h1>"+str(file.replace("fastas/","").replace(".fasta",""))+"</h1><a href=\"../rapport_bin.html\" target=\"_blank\"><input type=\"button\" value=\"Retour\"></a>")
 
 
-    message = "<table>\n<tbody>\n<tr>\n<td class = \"title\">Sequence</td>\n<td class = \"title\">MRNA</td>\n<td class = \"title\">Chain</td>\n<td class = \"title\">Mutation</td>\n</tr>\n"
-    
+      message = "<table>\n<tbody>\n<tr>\n<td class = \"title\">Sequence</td>\n<td class = \"title\">MRNA</td>\n<td class = \"title\">Chain</td>\n<td class = \"title\">Mutation</td>\n</tr>\n"
+      
 
-    read = m.read_file(file)
+      read = m.read_file(file)
 
-    sequence.append(DNA_bin.convert_to_binary(read,len(read)))
-    #print(len(sequence[i]))
+      sequence.append(DNA_bin.convert_to_binary(read,len(read)))
 
-    gene.append(DNA_bin.detecting_genes(array.array('l',sequence[i])))
+      gene.append(DNA_bin.detecting_genes(array.array('l',sequence[i])))
 
+      fh.write("<details><summary>"+str(file.replace("fastas/","").replace(".fasta",""))+"</summary>"+message+"<a href=\"sequences/"+str(file).replace("fastas/","")+"_bin.html\">"+str(file.replace("fastas/","").replace(".fasta",""))+"</a></details>")
 
-    #f.write("\nMRNA\t\t\t\t\t\t\t\tCHAIN\t\t\t\t\t\t\t\tMUTATION\n")
-    for j in range (len(gene[i])-1):
-        #print(str(file))
-        #print(str(sequence[i])+" "+str(gene[i][j][0])+" "+str(gene[i][j][1])+" "+str(gene[i][j][1]- gene[i][j][0]))
-        #seq = DNA_bin.get_piece_binary_array(array.array('l',sequence[i]),gene[i][j][0],gene[i][j][1]- gene[i][j][0])
-        #print("sequence = "+str(seq))
-
-        message += "<tr><td>sequence</td>\n"
-        res = (DNA_bin.generating_mRNA(array.array('l',sequence[i]),gene[i][j][0],gene[i][j][1]-gene[i][j][0]))
-        if res:
-            mrna.append(res.decode("cp1252", "replace"))
-            f.write(str(res.decode("cp1252", "replace")))
-            message+="<td>"+str(res.decode("cp1252", "replace"))+ "</td>\n"
-            #print(res.decode("cp1252", "replace"))
-        else:
-            message+="<td> none </td>\n"
-          
-
-        #print('aminoacid ='+str(sequence[i][gene[i][j][0]:(gene[i][j][1])]))
-        res2 =  DNA_bin.generating_amino_acid_chain(array.array('l',sequence[i]),gene[i][j][0],(gene[i][j][1]-gene[i][j][0])+1)
-
-        if res2:
-            chain.append(res2.decode("cp1252", "replace"))
-            f.write("\t\t\t\t\t\t\t\t"+str(res2.decode("cp1252", "replace")))
-            message+="<td>"+str(res2.decode("cp1252", "replace"))+ "</td>\n"
-        else:
-            message+="<td> none </td>\n"            
-        mutres = DNA_bin.detecting_mutations(array.array('l',sequence[i]),gene[i][j][0],gene[i][j][1]-gene[i][j][0])
-        mutation.append(mutres)
-        message+="<td>"+str(mutres)+ "</td>\n</tr>\n"
-        f.write("\t\t\t\t\t\t\t\t"+str(mutres)+"\n")
-    message+= "</tbody>\n</table>\n"
-
-    #print(str(gene))
+      for j in range(len(gene[i])-1):
 
 
+          res = (DNA_bin.generating_mRNA(array.array('l',sequence[i]),gene[i][j][0],gene[i][j][1]-gene[i][j][0]))
+          if res:
+              message += "<tr><td>"+res.decode("cp1252", "replace").replace("U","T")+"</td>\n"
 
-    f.write("\nMRNA:")
-    f.write(str(mrna))
-    f.write("\nCHAIN:")
-    f.write(str(chain))    
-    f.write("\nMUTATION:")
-    f.write(str(mutation))
-    message += "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
+              mrna.append(res.decode("cp1252", "replace"))
+              message+="<td>"+str(res.decode("cp1252", "replace"))+ "</td>\n"
+          else:
+              message += "<tr><td>["+str(gene[i][j][0])+":"+str(gene[i][j][1])+"]=</td>\n"
+              message+="<td> none </td>\n"
+            
 
-    f.write("\nMatching:")
-    if (len(gene[i])-1 > 2):
-        for j in range((int((len(gene[i]))/2)), -1, -1):
-            for k in range((int((len(gene[i]))/2))):     
-                #seq1 = DNA_bin.get_piece_binary_array(array.array('l',sequence[i]),gene[i][j][0],gene[i][j][1]- gene[i][j][0])
-                #seq2 = DNA_bin.get_piece_binary_array(array.array('l',sequence[i]),gene[i][k][0],gene[i][k][1]- gene[i][k][0])
+          res2 =  DNA_bin.generating_amino_acid_chain(array.array('l',sequence[i]),gene[i][j][0],(gene[i][j][1]-gene[i][j][0])+1)
 
-                res = DNA_bin.calculating_matching_score(array.array('l',sequence[i]),gene[i][j][0],(gene[i][j][1]- gene[i][j][0])+1,array.array('l',sequence[i]),gene[i][k][0],(gene[i][k][1]- gene[i][k][0])+1)
+          if res2:
+              chain.append(res2.decode("cp1252", "replace"))
+              message+="<td>"+str(res2.decode("cp1252", "replace"))+ "</td>\n"
+          else:
+              message+="<td> none </td>\n"            
+          mutres = DNA_bin.detecting_mutations(array.array('l',sequence[i]),gene[i][j][0],gene[i][j][1]-gene[i][j][0])
+          if len(mutres) == 0:
+            message+="<td>none</td>\n</tr>\n"
+          else:
+            message+="<td>"+str(mutres)+ "</td>\n</tr>\n"          
+          mutation.append(mutres)
 
-                message+="<tr><td>sequence</td>\n"
-                f.write("[sequence]=")
-                #res = (DNA_bin.calculating_matching_score(array.array('l',seq1),array.array('l',seq2)))
-                matching.append(res)
-                message+="<td>"+str(res)+ "</td> \n</tr>\n"
-                f.write(str(res)+"\t")
-    message+= "</tbody>\n</table>\n</details>\n </html>"  
+      message+= "</tbody>\n</table>\n"
+
+      message += "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
+
+      if (len(gene[i])-1 > 2):
+          for j in range((int((len(gene[i]))/2)), -1, -1):
+              for k in range((int((len(gene[i]))/2))):     
+
+                  res = DNA_bin.calculating_matching_score(array.array('l',sequence[i]),gene[i][j][0],(gene[i][j][1]- gene[i][j][0])+1,array.array('l',sequence[i]),gene[i][k][0],(gene[i][k][1]- gene[i][k][0])+1)
+
+                  message+="<tr><td>Sequence ["+str(gene[i][j][0])+":"+str(gene[i][j][1])+"] - ["+str(gene[i][k][0])+":"+str(gene[i][k][1])+"]</td>\n"
+                  matching.append(res)
+                  message+="<td>"+str(res)+ "</td> \n</tr>\n"
+      message+= "</tbody>\n</table>\n</details>\n </html>" 
+      fhtmp.write(message)
+      fhtmp.close()    
+
+      for c in range(i-1, -1, -1): 
+          fhtmp2 = open("output/sequences/cmp"+str(i)+"-"+str(c)+'_bin.html','w')
+          messagematch+="<details><summary>Sequence "+str(i)+" - "+str(c)+"</summary><a href=\"sequences/cmp"+str(i)+"-"+str(c)+"_bin.html\">Comparaison "+str(i)+"-"+str(c)+"</a></details>\n"
+
+          msgtmp = "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>"
+          for j in range(int((len(gene[i])))):
+            for k in range(len(gene[c])):
+                  res = DNA_bin.calculating_matching_score(array.array('l',sequence[i]),gene[i][j][0],(gene[i][j][1]- gene[i][j][0])+1,array.array('l',sequence[c]),gene[c][k][0],(gene[c][k][1]- gene[c][k][0])+1)       
+                  msgtmp+="<tr><td>Sequence ["+str(gene[i][j][0])+":"+str(gene[i][j][1])+"] - ["+str(gene[c][k][0])+":"+str(gene[c][k][1])+"]</td>\n"
+                  matching.append(res)
+                  msgtmp+="<td>"+str(res)+ "</td> \n</tr>\n"
+          fhtmp2.write(msgtmp)
+          fhtmp2.close()
+
+      i=i+1
+      if i ==2:
+        break
+  message= " <a href=\"comp_bin.html\">Comparaison séquences</a></details></html>"  
+  fh.write(message)
+  fh.close()
+
+  messagematch+= "</tbody>\n</table>\n\n </html>"  
 
 
-    fhtmp.write(message)
-    fhtmp.close()
-    i=i+1
-    if i ==4:
-      break
-message= " <a href=\"comp_bin.html\">Comparaison séquences</a></details></html>"  
-fh.write(message)
-fh.close()
-
-fh = open('output/comp_bin.html','w')
-fh.write(start+ "<h1>Comparaison entre séquences</h1><a href=\"rapport_bin.html\" target=\"_blank\"><input type=\"button\" value=\"Retour\"></a>")
-message = "<table>\n<tr>\n<th class = \"title\">Sequence</th>\n<th class = \"title\">Matching</th>\n</tr>\n<tbody>\n"
-#print(str(len(gene)))
-f.write("\nMatching:")
-for i in range((int((len(gene))/2)), -1, -1):
-    for c in range((int((len(gene))/2))):     
-        for j in range(int((len(gene[i])))):
-            for k in range(int((len(gene[c])))):
-                res = DNA_bin.calculating_matching_score(array.array('l',sequence[i]),gene[i][j][0],gene[i][j][1]- gene[i][j][0],array.array('l',sequence[c]),gene[c][k][0],gene[c][k][1]- gene[c][k][0])           
-                message+="<tr><td>sequence</td>\n"
-                f.write("[sequence]=")
-                matching.append(res)
-                message+="<td>"+str(res)+ "</td> \n</tr>\n"
-                f.write(str(res)+"\t")
-message+= "</tbody>\n</table>\n\n </html>"  
+  fhc.write(messagematch)
+  fhc.close()
 
 
-fh.write(message)
-fh.close()
-f.close()
 
+if __name__ == "__main__":
+    main()
