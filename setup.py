c458ab598e429afc88e494e8bfe20bd1448f8bab
Fiona Santoro <fsantoro39@gmail.com>
2022-01-26 00:03:27 +0100

    Style: Delete old files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-DNA_module = Extension("DNA", sources = [ "gene.c", "DNA.c" ])
-
-setup(name        = "DNA",
-      version     = "1.0",
-      description = "Genome analyser", 
-      ext_modules = [ DNA_module ])
