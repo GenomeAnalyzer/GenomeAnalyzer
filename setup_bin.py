21e71a20249089fb17816c3e6c58b60a7b803175
Alexandre Maheux <alexandre.maheux@ens.uvsq.fr>
2022-01-26 14:25:00 +0100

    Remove old files

--- setup_bin.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-DNAb_module = Extension("DNA_bin", sources = [ "gene_bin.c", "DNA_bin.c" ])
-
-setup(name        = "DNA_bin",
-      version     = "2.0",
-      description = "Genome analyser", 
-      ext_modules = [ DNAb_module ])
