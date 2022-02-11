802a4a77e9fb4ef0ce72f953e81fc077a4ac554b
Alexandre Maheux <alexandre.maheux@ens.uvsq.fr>
2022-02-12 00:28:19 +0100

    Update bool version from master
    Add new repository for bool version and new file in repo rdtsc and
    modification of Makefile in my way.

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
-$(shell mkdir bin/ output/)
-
 CC = gcc
 
 CFLAGS = -g -std=c99 -Wall
@@ -10,12 +8,15 @@ V0 = ./versions/naive
 
 V1 = ./versions/bin
 
+V2 = ./versions/bool
+
 BIN = bin
 
 OUTPUT = output
 
-.PHONY: clean all check
+$(shell mkdir -p bin/ output/)
 
+.PHONY: clean all check
 
 ifeq (run, $(firstword $(MAKECMDGOALS)))
   runargs := $(wordlist 2, $(words $(MAKECMDGOALS)), $(MAKECMDGOALS))
@@ -26,25 +27,28 @@ endif
 all: DNA DNA_bin check 
 
 #For only building and testing interface
-build: DNA DNA_bin
+build: DNA DNA_bin DNA_bool
 
 #Clean compilation files & output result
 clean :
 	find . -type d  -name "__pycache__" -exec rm -rv {} +
-	@rm -rf  build bin .pytest_cache output versions/bin/python/*.so output versions/naive/python/*.so
-	@rm -f $(OUTPUT)/*.o $(OUTPUT)/test_gene $(OUTPUT)/test_gene_bin
+	@rm -rf  build bin .pytest_cache output versions/bin/python/*.so output versions/naive/python/*.so versions/bool/python/*.so
 
 #For only executing tests
-check: run_test_gene test_DNA run_test_gene_bin test_DNA_bin
+check: run_test_gene test_DNA run_test_gene_bin test_DNA_bin test_DNA_bool
+
 
 #For only running the non-binary program
 run:
 	python3 $(V0)/python/main.py $(runargs)
 	
-
 #For only running the binary program
 run_bin:
 	python3 $(V1)/python/main_bin.py $(runargs)
+
+#For only running the boolean program
+run_bool:
+	python3 $(V2)/python/main_bool.py $(runargs)
 
 
 # Naïve library
@@ -56,7 +60,8 @@ run_test_gene: $(BIN)/test_gene
 
 DNA :
 	python3 $(V0)/python/setup.py build
-	cp build/lib*/*.so $(V0)/python/
+	cp build/lib*/DNA.*.so $(V0)/python/
+	cp build/lib*/DNA.*.so $(V0)/tests/
 
 test_DNA :
 	python3 -m pytest -s $(V0)/tests/test_DNA.py
@@ -71,7 +76,25 @@ run_test_gene_bin: $(BIN)/test_gene_bin
 
 DNA_bin : 
 	python3 $(V1)/python/setup_bin.py build
-	cp build/lib*/*.so $(V1)/python/
+	cp build/lib*/DNA_bin.*.so $(V1)/python/
+	cp build/lib*/DNA_bin.*.so $(V1)/tests/
+	cp build/lib*/DNA_bin.*.so mytests/
 
 test_DNA_bin : 
 	python3 -m pytest -s $(V1)/tests/test_DNA_bin.py
+
+
+# Binary optimized library
+$(BIN)/test_gene_bool : $(V2)/tests/test_gene_bool.c 
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+run_test_gene_bool: $(BIN)/test_gene_bool
+	./$(BIN)/test_gene_bin &
+
+DNA_bool : 
+	python3 $(V2)/python/setup_bool.py build
+	cp build/lib*/DNA_bool.*.so $(V2)/python/
+	cp build/lib*/DNA_bool.*.so $(V2)/tests/
+
+test_DNA_bool : 
+	python3 -m pytest -s $(V2)/tests/test_DNA_bool.py
---
+++ versions/bool/src/gene_bool.c
@@ -0,0 +1,631 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "../headers/gene_bool.h"
+
+
+/***************************************/
+/********** BINARIES FUNCTION **********/
+/***************************************/
+
+
+
+/**
+ * Popcount a binary array sequence.
+ * 
+ * in : seq_bin : sequence in binary array format
+ * in : seq_size : seq_bin length (number total of used bits)
+ * out : bin_popcount : popcount of the seq : number of '1'
+ * 
+ * Iterates on seq_bin and for each value, adds its popcount to bin_popcount.
+ */
+int popcount_binary_array(const _Bool *seq_bin, const long int seq_size){
+    int bin_popcount = 0;
+
+    // Find the size of the binary array
+    long int array_size = seq_size / int_SIZE;
+    if(seq_size % int_SIZE != 0) array_size++;
+
+    // Parse the binary array
+    for (long int i = 0; i < array_size; ++i)
+        bin_popcount += __builtin_popcount(seq_bin[i]);
+
+    return bin_popcount;
+}
+
+
+
+
+/***************************************/
+/******** DNA & GENES FUNCTION *********/
+/***************************************/
+
+//////////////// Convert to binary
+/**
+ * Convert a DNA base sequence to its binary array format.
+ * 
+ * in : dna_seq : DNA sequence in char array format
+ * in : size : dna_seq length = number of nucleotides (= number of letters)
+ * out : seq : DNA sequence in binary array format
+ * 
+ * Calls set_binary_array.
+ */
+_Bool *convert_to_binary(const char *dna_seq, const unsigned size){
+
+
+    // Allocate memory and verify it has been allocated
+    _Bool *seq_bin = NULL;
+    seq_bin = calloc(size*2, sizeof(_Bool));
+    if(!seq_bin)
+        return printf("ERROR: set_binary_array: cannot allocate memory.\n"), NULL;
+
+    int pos = 0;
+    // Parse the DNA sequence, per nucleotides
+    for (long int i = 0; i < size; ++i){
+        //printf("%c ",dna_seq[i]);
+        // Set seq_bin bit values according to the nucleotide read
+        switch(dna_seq[i]){
+        case 'A': // A = 00
+            break;
+        case 'T': // T = 11
+            seq_bin[pos] = 1;
+            seq_bin[pos+1] = 1;
+            break;
+        case 'G': // G = 01
+            seq_bin[pos+1] = 1;
+            break;
+        case 'C': // C = 10
+            seq_bin[pos] = 1;
+            break;
+        case 'N': // N = A = 00
+            break;
+        case 'R': // R = A = 00
+            break;
+        case 'Y': // Y = C = 10
+            seq_bin[pos] = 1;
+            break;
+        case 'K': // K = G = 01
+            seq_bin[pos+1] = 1;
+            break;
+        case 'M': // M = A = 00
+            break;
+        case 'S': // S = C = 10
+            seq_bin[pos] = 1;
+            break;
+        case 'W': // W = A = 00
+            break;
+        case 'B': // B = C = 10
+            seq_bin[pos] = 1;
+            break;
+        case 'D': // D = A = 00
+            break;
+        case 'H': // H = G = 01
+            seq_bin[pos+1] = 1;
+            break;
+        case 'V': // V = A = 00
+            break;
+        }
+        pos += 2;
+    }
+    return seq_bin;
+}
+
+//////////////// Convert binary aa to codon
+/**
+ * Convert a DNA sequence in binary array format to its DNA bases.
+ * 
+ * in : bin_dna_seq : DNA sequencDNA sequence in binary array format
+ * in : size : total number total of used bits in the sequence bin_dna_seq
+ * out : dna_seq : DNA sequence in char array format
+ * 
+ * For each pair of bits in bin_dna_seq, append to dna_seq its corresponding nucleotide.
+ */
+char* binary_to_dna(_Bool* bin_dna_seq, const unsigned size){
+    if (size % 2 != 0) {
+        printf("Error: binary_to_aa : wrong binary size (%d). Must be odd.\nExit.\n",size);
+        return NULL;
+    }
+
+    //Allocate memory and verify it has been allocated
+    char* dna_seq = calloc((size / 2) + 1, sizeof(*dna_seq));
+    if(!dna_seq)
+        return printf("ERROR: binary_to_dna: cannot allocate memory.\n"), NULL;
+
+    int j = 0;
+    //Parse the binary array, two bits per iteration
+    for (unsigned i = 0; i < size; i += 2){
+        // nucleotides = A, T, G, C
+        int nucl1 = bin_dna_seq[i];
+        int nucl2 = bin_dna_seq[i + 1];
+
+        if (nucl1 == 0 && nucl2 == 0)
+            // 00 = A/N
+            dna_seq[j] = 'A';
+        else if (nucl1 == 1 && nucl2 == 1)
+            // 11 = T
+            dna_seq[j] = 'T';
+        else if (nucl1 == 1 && nucl2 == 0)
+            // 10 = C
+            dna_seq[j] = 'C';
+        else if (nucl1 == 0 && nucl2 == 1)
+            // 01 = G
+            dna_seq[j] = 'G';
+        j++;
+    }
+    return dna_seq;
+}
+
+//////////////// Generating mRNA
+/**
+ * Convert a DNA sequence in binary array format to its mRNA sequence.
+ * 
+ * in : gene_seq : DNA sequence in binary array format
+ * in : seq_size : number total of used bits in the sequence gene_seq
+ * out : rna_seq : resulting mRNA sequence in char array format
+ * Convert a binary DNA sequence to a string mRNA sequence
+ * 
+ * For each pair of bits in bin_dna_seq, append to dna_seq its corresponding nucleotide in mRNA. (T -> U)
+ */
+char* generating_mRNA(const _Bool* gene_seq, const long int seq_size) {
+    if (!gene_seq)
+        return printf("ERROR: generating_mRNA: undefined sequence\n"), NULL;
+
+    // Create and check the output
+    char* rna_seq = NULL;
+    rna_seq = malloc(sizeof(*rna_seq) * (seq_size / 2) + 2);
+    if (!rna_seq)
+        return printf("ERROR: generating_mRNA: cannot allocate memory\n"), NULL;
+
+
+    int j = 0;
+    // Parse the binary DNA sequence two by two
+    for (unsigned i = 0; i < seq_size; i += 2) {
+        switch (gene_seq[i]) {
+        case 0:
+            if (gene_seq[i + 1] == 0)
+                // A = 00
+                rna_seq[j] = 'A';
+            else if (gene_seq[i + 1] == 1)
+                // G = 01
+                rna_seq[j] = 'G';
+            break;
+        case 1:
+            if (gene_seq[i + 1] == 0)
+                // C = 10
+                rna_seq[j] = 'C';
+            else if (gene_seq[i + 1] == 1)
+                // U = 11
+                rna_seq[j] = 'U';
+            break;
+        default:
+            return printf("ERROR: generating_mRNA: invalid value in DNA sequence\n"), NULL;
+        }
+        j++;
+    }
+    rna_seq[j] = '\0';
+    return rna_seq;
+}
+
+//////////////// Detecting genes 
+/**
+ * Detects genes in the mRNA sequence in binary array format and maps them.
+ * 
+ * in : gene : mRNA sequence in binary array format
+ * in : gene_size : number total of used bits in the sequence gene
+ * in : gene_map : gene mapping struct
+ * out : void
+ * 
+ * Iterates in the gene, and for each packet of 6 binary bits (corresponding to a nucleotide), searches for a start codon (AUG).
+ * If a start codon is found, iterate until a stop codon is found (UAA, UAG or UGA).
+ * If a stop codon is found, append to gene_map the gene length (from start to stop codon), its start position and stop one.
+ * 
+ * NB : The gene in binary array form can correspond to an mRNA or DNA sequence, since it is stored in the same way.
+ */
+void detecting_genes(const _Bool* gene, const long int gene_size, gene_map_t* gene_map) {
+    gene_map->genes_counter = 0;
+
+    // Check if memory ever have been allocated and allocate it if not
+    if(!gene_map->gene_start || !gene_map->gene_end){
+        gene_map->gene_start = malloc(sizeof(*gene_map->gene_start) * MAX_GENES);
+        gene_map->gene_end = malloc(sizeof(*gene_map->gene_end) * MAX_GENES);
+
+        if (!gene_map->gene_start || !gene_map->gene_end){
+            printf("ERROR: detecting_genes: cannot allocate memory\n");
+            return;
+        }
+    }
+
+    int start_pos = -1;
+
+    long int i = 0;
+
+    //Parse the binary array, and find all the start and stop codons
+    while ((i + 6) <= gene_size) {
+        // Each nucleotides can be A, U, G or C
+        int nucl1 = gene[i];
+        int nucl2 = gene[i + 1];
+        int nucl3 = gene[i + 2];
+        int nucl4 = gene[i + 3];
+        int nucl5 = gene[i + 4];
+        int nucl6 = gene[i + 5];
+       // printf("%d %d %d %d %d %d\n",nucl1,nucl2,nucl3,nucl4,nucl5,nucl6);
+
+
+        //If a start pos and a stop pos doesn't exist, search for AUG
+        if (nucl1 == 0 && nucl2 == 0 && nucl3 == 1 
+            && nucl4 == 1 && nucl5 == 0 && nucl6 == 1) {
+        //if AUG, it's the start of a gene
+            start_pos = i;
+            i += 6;
+        }
+        else{
+
+            if (start_pos != -1 ) {
+                //if a start pos exists , search for UAA / UAG / UGA
+                if ((nucl1 == 1 && nucl2 == 1 && nucl3 == 0) 
+                    && ((nucl4 == 0 && nucl5 == 0 && nucl6 == 0)
+                        || (nucl4 == 0 && nucl5 == 0 && nucl6 == 1)
+                        || (nucl4 == 1 && nucl5 == 0 && nucl6 == 0))) {
+                   //It's the end of a gene          
+                   //If a start pos and an stop pos has been found, a gene exists so we save it in the struc
+                    gene_map->gene_start[gene_map->genes_counter] = start_pos;
+                    gene_map->gene_end[gene_map->genes_counter] = i+5;
+
+                    gene_map->genes_counter++;
+
+                    start_pos = -1;
+                    i += 6;
+                }
+                else
+                    i += 2;
+            }
+            else
+                i += 2;
+        }
+    }
+
+}
+
+/**
+ * Retrives amino acid chains in a mRNA sequence in binary array format.
+ * 
+ * in : gene_seq : DNA sequence in binary array format
+ * in : seq_size : gene_seq length (number total of used bits)
+ * out : aa_seq : char array of proteins symbols.
+ * 
+ * The program parses the mRNA sequence, verify its length (seq_size).
+ * Then iterates on gene_seq and for each packet of 6 binary bits (corresponding to a nucleotide), append to aa_seq its corresponding protein symbol.
+ * 
+ * NB : The gene in binary array form can correspond to an mRNA or DNA sequence, since it is stored in the same way.
+*/
+char* generating_amino_acid_chain(const _Bool*gene_seq, const long int seq_size) {
+    long int codon_size = 6;
+    // Check the input argument
+    if (!gene_seq)
+        return printf("ERROR: generating_amino_acid_chain: undefined sequence\n"), NULL;
+    if(seq_size % 3 != 0)
+        return NULL;
+
+    // Allocate memory and verify it has been allocated
+    char* aa_seq = NULL;
+     aa_seq = malloc(sizeof(*aa_seq) * (seq_size / codon_size) + 1);
+    if (!aa_seq)
+        return printf("ERROR: generating_amino_acid_chain: cannot allocate memory\n"), NULL;
+
+    unsigned temp = 0;
+
+    long size = seq_size;
+
+    //Parse the binary array, six bits by six (to parse three nucleotides per three)
+    for (long int i = 0; i < size; i += codon_size) {
+        // The hash functions, takes the 6 bits, and transform the array into an integer.
+        // The integer first char is a 2, for hash generation purposes.
+        int hash = 2;
+        for(long int k = i; k < i + codon_size; k++){
+            hash = 10 * hash + gene_seq[k];
+        }
+
+        // Switch over the hash.
+        switch(hash){
+        case 2000000 :
+            aa_seq[temp] = 'K';
+            break;
+        case 2000001 :
+            aa_seq[temp] = 'K';
+            break;
+        case 2000010 :
+            aa_seq[temp] = 'N';
+            break;
+        case 2000011 :
+            aa_seq[temp] = 'N';
+            break;
+        case 2000100 :
+            aa_seq[temp] = 'R';
+            break;
+        case 2000101 :
+            aa_seq[temp] = 'R';
+            break;
+        case 2000110 :
+            aa_seq[temp] = 'S';
+            break;
+        case 2000111 :
+            aa_seq[temp] = 'S';
+            break;
+        case 2001000 :
+            aa_seq[temp] = 'T';
+            break;
+        case 2001001 :
+            aa_seq[temp] = 'T';
+            break;
+        case 2001010 :
+            aa_seq[temp] = 'T';
+            break;
+        case 2001011 :
+            aa_seq[temp] = 'T';
+            break;
+        case 2001100 :
+            aa_seq[temp] = 'I';
+            break;
+        case 2001101 :
+            aa_seq[temp] = 'M';
+            break;
+        case 2001110 :
+            aa_seq[temp] = 'I';
+            break;
+        case 2001111 :
+            aa_seq[temp] = 'I';
+            break;
+        case 2010000 :
+            aa_seq[temp] = 'E';
+            break;
+        case 2010001 :
+            aa_seq[temp] = 'E';
+            break;
+        case 2010010 :
+            aa_seq[temp] = 'D';
+            break;
+        case 2010011 :
+            aa_seq[temp] = 'D';
+            break;
+        case 2010100 :
+            aa_seq[temp] = 'G';
+            break;
+        case 2010101 :
+            aa_seq[temp] = 'G';
+            break;
+        case 2010110 :
+            aa_seq[temp] = 'G';
+            break;
+        case 2010111 :
+            aa_seq[temp] = 'G';
+            break;
+        case 2011000 :
+            aa_seq[temp] = 'A';
+            break;
+        case 2011001 :
+            aa_seq[temp] = 'A';
+            break;
+        case 2011010 :
+            aa_seq[temp] = 'A';
+            break;
+        case 2011011 :
+            aa_seq[temp] = 'A';
+            break;
+        case 2011100 :
+            aa_seq[temp] = 'V';
+            break;
+        case 2011101 :
+            aa_seq[temp] = 'V';
+            break;
+        case 2011110 :
+            aa_seq[temp] = 'V';
+            break;
+        case 2011111 :
+            aa_seq[temp] = 'V';
+            break;
+        case 2100000 :
+            aa_seq[temp] = 'Q';
+            break;
+        case 2100001 :
+            aa_seq[temp] = 'Q';
+            break;
+        case 2100010 :
+            aa_seq[temp] = 'H';
+            break;
+        case 2100011 :
+            aa_seq[temp] = 'H';
+            break;
+        case 2100100 :
+            aa_seq[temp] = 'R';
+            break;
+        case 2100101 :
+            aa_seq[temp] = 'R';
+            break;
+        case 2100110 :
+            aa_seq[temp] = 'R';
+            break;
+        case 2100111 :
+            aa_seq[temp] = 'R';
+            break;
+        case 2101000 :
+            aa_seq[temp] = 'P';
+            break;
+        case 2101001 :
+            aa_seq[temp] = 'P';
+            break;
+        case 2101010 :
+            aa_seq[temp] = 'P';
+            break;
+        case 2101011 :
+            aa_seq[temp] = 'P';
+            break;
+        case 2101100 :
+            aa_seq[temp] = 'L';
+            break;
+        case 2101101 :
+            aa_seq[temp] = 'L';
+            break;
+        case 2101110 :
+            aa_seq[temp] = 'L';
+            break;
+        case 2101111 :
+            aa_seq[temp] = 'L';
+            break;
+        case 2110000 :
+            aa_seq[temp] = 'O';
+            break;
+        case 2110001 :
+            aa_seq[temp] = 'O';
+            break;
+        case 2110010 :
+            aa_seq[temp] = 'Y';
+            break;
+        case 2110011 :
+            aa_seq[temp] = 'Y';
+            break;
+        case 2110100 :
+            aa_seq[temp] = 'O';
+            break;
+        case 2110101 :
+            aa_seq[temp] = 'W';
+            break;
+        case 2110110 :
+            aa_seq[temp] = 'C';
+            break;
+        case 2110111 :
+            aa_seq[temp] = 'C';
+            break;
+        case 2111000 :
+            aa_seq[temp] = 'S';
+            break;
+        case 2111001 :
+            aa_seq[temp] = 'S';
+            break;
+        case 2111010 :
+            aa_seq[temp] = 'S';
+            break;
+        case 2111011 :
+            aa_seq[temp] = 'S';
+            break;
+        case 2111100 :
+            aa_seq[temp] = 'L';
+            break;
+        case 2111101 :
+            aa_seq[temp] = 'L';
+            break;
+        case 2111110 :
+            aa_seq[temp] = 'F';
+            break;
+        case 2111111 :
+            aa_seq[temp] = 'F';
+            break;
+
+        default:
+            return NULL;
+        }
+    
+        temp++;
+    }
+
+    aa_seq[temp] = '\0';
+    return aa_seq;
+}
+
+
+/**
+ * Detects probable mutation areas.
+ * 
+ * in : gene_seq : DNA sequence in binary array format
+ * in : size_sequence : gene_seq length (number total of used bits)
+ * in : mut_m : map of the possible mutation's areas
+ * out : void
+ * 
+ * The algorithm scans a gene sequence and locates the high frequency of GC DNA bases in the sequence.
+ * Must be at least 1/5th of the gene length
+ * Precondition: gene_seq is of size size_sequence.
+ * 
+ * NB : The gene in binary array form can correspond to an mRNA or DNA sequence, since it is stored in the same way.
+ */
+void detecting_mutations(const _Bool*gene_seq, const long int size_sequence,
+                         mutation_map mut_m) {
+    long int detect_mut = 0;  //Counting size of GC sequence
+    unsigned short tmp_start_mut = 0;   //stock start mutation
+    unsigned cmp = 0;   //counter of all mutation zones
+
+    long size = size_sequence;
+    //Parse the binary array, from the 'start_pos' bit to the end
+    for (long int i = 0; i < size; i += 2) {
+
+        // each nucleotides can be  A, U, G or C
+        int nucl1 = gene_seq[i];
+        int nucl2 = gene_seq[i + 1];
+
+        //Increment detect_mut if find a C or G nucl
+        if (((nucl1 == 0) && (nucl2 == 1)) ||
+            ((nucl1 == 1) && (nucl2 == 0))) {
+            if(detect_mut == 0){tmp_start_mut = i;}
+            detect_mut+=2;
+        }
+        //Put detect_mut to 0 if find a A or T nucl
+        else {
+            //Check if previous GC sequence is a probable mutation zone
+            if (detect_mut >= (size_sequence / 5)) {
+                mut_m.start_mut[cmp] = tmp_start_mut;
+                mut_m.end_mut[cmp] = (i-1);
+                mut_m.size[cmp] = detect_mut-1;
+                cmp++;
+            }
+            detect_mut = 0;
+        }
+    }
+    //Check if ending sequence is a probable mutation zone
+    if (detect_mut >= (size_sequence / 5)) {
+        mut_m.start_mut[cmp] = tmp_start_mut;
+        mut_m.end_mut[cmp] = size_sequence-1;
+        mut_m.size[cmp] = detect_mut-1;
+    }
+}
+
+/*
+ * in : seq1 : first sequence in binary
+ * in : seq2 : second sequence in binary
+ * out : float : matching score in %
+ * The algorithms runs the hamming distance between two binary sequences, and return their matching score in %
+*/
+float calculating_matching_score(const _Bool seq1 [], const int sequence_size1,
+                                 const _Bool seq2 [], const int sequence_size2) {
+    // Check the input argument
+    if (!seq1 || !seq2)
+        return printf("ERROR: calculating_matching_score: undefined sequence\n"), -1.0;
+
+    // If the sequences don't have the same size, do (with x = 1 or 0):
+
+    //  xxxxxxxxx
+    // ^
+    //  000xxxxxx
+    // -----------
+    //  xxxxxxxxx
+
+    // And, 0 ^ x = x
+
+    //Find the greater sequence and rename both
+    const _Bool* s1 = sequence_size1 >= sequence_size2 ? seq1 : seq2;
+    const _Bool* s2 = sequence_size1 >= sequence_size2 ? seq2 : seq1;
+    int diff_size = sequence_size1 >= sequence_size2 ? sequence_size1 - sequence_size2 : sequence_size2 - sequence_size1;
+    int max_size = sequence_size1 >= sequence_size2 ? sequence_size1 : sequence_size2;
+    int count = 0;
+
+    // For the greater part of the greater sequence, count the number of '1'
+    for (int i = 0; i < diff_size; i++)
+        if (s1[i]) ++count;
+
+    // Count the number of '1', after aplying the xor operation
+    for (int i = diff_size; i < max_size; ++i) {
+        if (s1[i] ^ s2[i - diff_size]) ++count;
+    }
+
+    // Compute and return the percentage
+    float y = ((float)count * 100.0) / (float)max_size;
+    return 100.0 - y;
+}
