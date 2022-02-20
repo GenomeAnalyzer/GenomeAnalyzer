4a9e98a2f6456e9c49cb88df36ce7d6c30124fca
Fiona Santoro <fsantoro39@gmail.com>
2022-02-20 19:53:32 +0100

    Feat: Add flag to python version

--- versions/bin/python/setup_bin.py
+++ versions/bin/python/setup_bin.py
@@ -1,6 +1,19 @@
 from distutils.core import setup, Extension
+import os
+import sysconfig
 
-DNAb_module = Extension("DNA_bin", sources = [ "./versions/bin/src/gene_bin.c", "./versions/bin/src/DNA_bin.c" ])
+_DEBUG = False
+_DEBUG_LEVEL = 0
+
+# Common flags for both release and debug builds.
+extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
+extra_compile_args += [ "-Wall", "-Wextra","-fopenmp"]
+if _DEBUG:
+    extra_compile_args += ["-g3", "-O0", "-DDEBUG=%s" % _DEBUG_LEVEL, "-UNDEBUG"]
+else:
+    extra_compile_args += ["-DNDEBUG", "-O3"]
+    
+DNAb_module = Extension("DNA_bin", sources = [ "./versions/bin/src/gene_bin.c", "./versions/bin/src/DNA_bin.c" ],extra_compile_args = extra_compile_args)
 
 setup(name        = "DNA_bin",
       version     = "2.0",
