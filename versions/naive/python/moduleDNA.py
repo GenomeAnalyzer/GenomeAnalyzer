68e10403b03df3616bba100495eb27fb3270e3a2
ZzDaBing <guillaume.bigand@ens.uvsq.fr>
2022-02-20 08:13:37 +0100

    Minor fix in moduleDNA.py for the three versions
    ->Some changes for a cleaner code

--- versions/naive/python/moduleDNA.py
+++ versions/naive/python/moduleDNA.py
@@ -1,12 +1,4 @@
 def read_file(name):
-    f = open(name,"r")
-    f.readline()
-    gene = ""
-    while 1:
-        char = f.readline()
-        if not char:
-            f.close()
-            break
-        gene= gene +char.strip()
-    f.close()
-    return gene
+    f = open(name, "r")
+    fr = f.readlines()
+    return "".join([f[:-1] for f in fr[1:]])
